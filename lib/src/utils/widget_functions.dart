a5146306f2100b36e6ccb09cd022e9be31173e77
Jemal <53106252+Jebdi@users.noreply.github.com>
2021-08-25 14:32:28 -0500

    Custom App Bar standardized

--- lib/src/utils/widget_functions.dart
+++ lib/src/utils/widget_functions.dart
@@ -9,6 +9,14 @@ Widget addHorizontalSpace(double width) {
   return SizedBox(width: width);
 }
 
+AppBar customAppBar() {
+  return AppBar(
+    automaticallyImplyLeading: true,
+    iconTheme: IconThemeData(color: COLOR_YELLOW),
+    elevation: 0,
+  );
+}
+
 Container oddaaImage() {
   return Container(
     width: 60,
