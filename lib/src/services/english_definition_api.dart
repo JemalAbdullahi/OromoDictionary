3f1eeebeb994411d9c3c5acf117f200fb96f61f8
Jemal <53106252+Jebdi@users.noreply.github.com>
2021-08-25 14:33:43 -0500

    Remove hardcoded data

--- lib/src/services/english_definition_api.dart
+++ lib/src/services/english_definition_api.dart
@@ -16,17 +16,6 @@ class EnglishDefinitionAPI {
   static Future<void> fetchEnglishDefinition(EnglishWord englishWord) async {
     resetPathSeg();
     pathSeg.add(englishWord.word);
-    print(dictionaryURL.replace(pathSegments: pathSeg));
-    // List<String> definitions = [];
-    englishWord.audio =
-        "//ssl.gstatic.com/dictionary/static/sounds/20200429/aback--_gb_1.mp3";
-    englishWord.definitions = {
-      "definitions": [
-        "towards or situated to the rear; back.",
-        "with the sail pressed backwards against the mast by a headwind.",
-      ]
-    };
-    print(englishWord.definitions.toString());
     final Response response = await client
         .get(dictionaryURL.replace(pathSegments: pathSeg))
         .timeout(Duration(seconds: 5), onTimeout: () {
