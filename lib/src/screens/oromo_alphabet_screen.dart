950fb3ce3406413419107f29d6665677bb05b806
Jemal <53106252+Jebdi@users.noreply.github.com>
2021-08-25 14:32:10 -0500

    Add all letter descritions

--- lib/src/screens/oromo_alphabet_screen.dart
+++ lib/src/screens/oromo_alphabet_screen.dart
@@ -11,6 +11,13 @@ class OromoAlphabetScreen extends StatefulWidget {
 
 class _OromoAlphabetScreenState extends State<OromoAlphabetScreen> {
   // ThemeData? theme;
+  String selectedLetters = "";
+  Map<String, Map<String, String>> letterSet = {
+    "Consonants": consonants,
+    "Oromo Specific Consonants": uniqueConsonants,
+    "Short Vowels": shortVowels,
+    "Long Vowels": longVowels,
+  };
   @override
   Widget build(BuildContext context) {
     ThemeData theme = Theme.of(context);
@@ -24,6 +31,18 @@ class _OromoAlphabetScreenState extends State<OromoAlphabetScreen> {
         ),
         centerTitle: true,
         iconTheme: theme.iconTheme,
+        actions: [
+          selectedLetters.isNotEmpty
+              ? IconButton(
+                  onPressed: () {
+                    setState(() {
+                      selectedLetters = "";
+                    });
+                  },
+                  icon: Icon(Icons.clear),
+                )
+              : SizedBox.shrink()
+        ],
       ),
       body: LayoutBuilder(
         builder: (context, constraints) {
@@ -31,28 +50,63 @@ class _OromoAlphabetScreenState extends State<OromoAlphabetScreen> {
             color: theme.primaryColor,
             height: constraints.maxHeight,
             width: constraints.maxWidth,
-            child: ListView.builder(
-              itemBuilder: (context, index) => Card(
-                color: theme.accentColor,
-                child: ListTile(
-                  leading: Text(
-                    consonants.keys.elementAt(index),
-                    style: theme.textTheme.headline6!
-                        .apply(color: theme.primaryColor),
-                  ),
-                  title: Text(
-                    consonants.values.elementAt(index),
-                    style: theme.textTheme.bodyText1!
-                        .apply(color: theme.primaryColor),
-                    softWrap: true,
-                  ),
-                ),
-              ),
-              itemCount: consonants.length,
-            ),
+            child: selectedLetters.isEmpty
+                ? _buildlistViewSelector(theme)
+                : _listViewLetterBuilder(theme, letterSet[selectedLetters]!),
           );
         },
       ),
+    );
+  }
+
+  ListView _buildlistViewSelector(ThemeData theme) {
+    return ListView(
+      children: [
+        _listTileCard(theme, letterSet.keys.elementAt(0)),
+        _listTileCard(theme, letterSet.keys.elementAt(1)),
+        _listTileCard(theme, letterSet.keys.elementAt(2)),
+        _listTileCard(theme, letterSet.keys.elementAt(3)),
+      ],
+    );
+  }
+
+  GestureDetector _listTileCard(ThemeData theme, String title) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selectedLetters = title;
+        });
+      },
+      child: Card(
+        color: theme.accentColor,
+        child: ListTile(
+          trailing: Icon(Icons.arrow_forward_ios, color: theme.primaryColor),
+          title: Text(title,
+              style:
+                  theme.textTheme.bodyText1!.apply(color: theme.primaryColor)),
+        ),
+      ),
+    );
+  }
+
+  ListView _listViewLetterBuilder(
+      ThemeData theme, Map<String, String> letters) {
+    return ListView.builder(
+      itemBuilder: (context, index) => Card(
+        color: theme.accentColor,
+        child: ListTile(
+          leading: Text(
+            letters.keys.elementAt(index),
+            style: theme.textTheme.headline6!.apply(color: theme.primaryColor),
+          ),
+          title: Text(
+            letters.values.elementAt(index),
+            style: theme.textTheme.bodyText1!.apply(color: theme.primaryColor),
+            softWrap: true,
+          ),
+        ),
+      ),
+      itemCount: letters.length,
     );
   }
 }
