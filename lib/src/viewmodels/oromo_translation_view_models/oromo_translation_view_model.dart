318bfaa28a40740349ead66e3a3daab17f332d23
Jemal <53106252+Jebdi@users.noreply.github.com>
2021-08-25 14:52:33 -0500

    fix translation breakdown

--- lib/src/viewmodels/oromo_translation_view_models/oromo_translation_view_model.dart
+++ lib/src/viewmodels/oromo_translation_view_models/oromo_translation_view_model.dart
@@ -23,34 +23,35 @@ class OromoTranslationViewModel {
     StringBuffer sb = new StringBuffer();
     String letter;
     for (int i = 0; i < translation.length; i++) {
-      if (i == translation.length - 1) {
-        letter = translation[i];
-      } else {
-        letter = translation.substring(i, i + 2);
-      }
-      if (uniqueConsonants.containsKey(letter)) {
-        sb.write(letter + ": ");
-        sb.write(uniqueConsonants[letter]!);
-        i++;
-      } else if (longVowels.containsKey(letter)) {
-        sb.write(letter + ": ");
-        sb.write(longVowels[letter]!);
-        i++;
-      } else if (shortVowels.containsKey(letter[0])) {
-        sb.write(letter[0] + ": ");
-        sb.write(shortVowels[letter[0]]!);
-      } else if (consonants.containsKey(letter[0])) {
-        if (letter[0] == letter[1]) {
-          sb.write("(Longer Sound) " + letter + ": ");
+      if (translation[i] != " ") {
+        int j = i < translation.length - 1 ? i + 1 : -1;
+        letter = j != -1 ? translation[i] + translation[j] : translation[i];
+        letter.trim();
+        print(letter);
+        if (uniqueConsonants.containsKey(letter)) {
+          sb.write(letter + ": ");
+          sb.write(uniqueConsonants[letter]!);
           i++;
-        } else {
+        } else if (longVowels.containsKey(letter)) {
+          sb.write(letter + ": ");
+          sb.write(longVowels[letter]!);
+          i++;
+        } else if (shortVowels.containsKey(letter[0])) {
           sb.write(letter[0] + ": ");
+          sb.write(shortVowels[letter[0]]!);
+        } else if (consonants.containsKey(letter[0])) {
+          if (letter.length == 2 && letter[0] == letter[1]) {
+            sb.write("(Longer Sound) " + letter + ": ");
+            i++;
+          } else {
+            sb.write(letter[0] + ": ");
+          }
+          sb.write(consonants[letter[0]]!);
         }
-        sb.write(consonants[letter[0]]!);
-      }
 
-      if (i < translation.length - 1) {
-        sb.write(",     ");
+        if (i < translation.length - 1) {
+          sb.write(",     ");
+        }
       }
     }
     return sb.toString();
